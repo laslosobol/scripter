0a6a0c3da49455e1abc19b121fcb3723898902ec
laslosobol <45056502+laslosobol@users.noreply.github.com>
2020-08-31 20:01:22 +0300

    Initial Commit feat: recognition of 3 pictures types, NewItem picture moves

---
+++ Scripter/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Scripter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.analisysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1424, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.opemToolStripMenuItem, this.analisysToolStripMenuItem, this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // opemToolStripMenuItem
+            // 
+            this.opemToolStripMenuItem.Name = "opemToolStripMenuItem";
+            this.opemToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.opemToolStripMenuItem.Text = "Open";
+            this.opemToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // analisysToolStripMenuItem
+            // 
+            this.analisysToolStripMenuItem.Name = "analisysToolStripMenuItem";
+            this.analisysToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.analisysToolStripMenuItem.Text = "Analisys";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(15, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(988, 811);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(988, 811);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(1009, 37);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(403, 275);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(3, 148);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(397, 120);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(397, 139);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.AutoScroll = true;
+            this.panel3.Controls.Add(this.richTextBox1);
+            this.panel3.Location = new System.Drawing.Point(1012, 318);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(399, 529);
+            this.panel3.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(399, 529);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1424, 861);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+        private System.Windows.Forms.PictureBox pictureBox3;
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.Panel panel2;
+
+        private System.Windows.Forms.ToolStripMenuItem analisysToolStripMenuItem;
+
+        private System.Windows.Forms.Panel panel1;
+
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripMenuItem opemToolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+
+        #endregion
+    }
+}
---
+++ Scripter/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.OCR;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using OpenTK.Graphics.OpenGL;
+
+namespace Scripter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var request = WebRequest.Create("http://185.80.129.249:4222/getImage");
+            var response = request.GetResponse();
+            var stream = response.GetResponseStream();
+            pictureBox1.Image = Image.FromStream(stream);
+            pictureBox2.Image = Image.FromFile(@"F:\Working Pack\c#\Scripter\Scripter\Templates\Customise.png");
+            pictureBox3.Image = Image.FromFile(@"F:\Working Pack\c#\Scripter\Scripter\Templates\NewItem.png");
+            var img = new Bitmap(pictureBox1.Image)
+                .ToImage<Gray, byte>();
+            var customiseTemplate = new Bitmap(pictureBox2.Image)
+                .ToImage<Gray, byte>();
+            var newItemTemplate = new Bitmap(pictureBox3.Image)
+                .ToImage<Gray, byte>();
+
+            Mat imgOut = new Mat();
+            if (DetectObject(img, customiseTemplate))
+            {
+                richTextBox1.AppendText("Customise\n");
+            }
+
+            else if (DetectObject(img, newItemTemplate))
+            {
+                richTextBox1.AppendText("NewItem\n");
+                if (DetectObject(img, new Bitmap(@"F:\Working Pack\c#\Scripter\Scripter\Templates\SendTo.png")
+                    .ToImage<Gray, byte>()))
+                {
+                    richTextBox1.AppendText("Send to transfer list: 1 move down!\n\n");
+                }
+                else if (DetectObject(img, new Bitmap(@"F:\Working Pack\c#\Scripter\Scripter\Templates\QuickSell.png")
+                    .ToImage<Gray, byte>()))
+                {
+                    richTextBox1.AppendText("Quick sell now: 1 move up!\n\n");
+                }
+                else
+                {
+                    //TextRecogniser(pictureBox1.Image);
+                    richTextBox1.AppendText("Keep Items:");
+                }
+            }
+            else
+            {
+                richTextBox1.AppendText("Not Appropriate for analysis!\n");
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private bool DetectObject(Image<Gray, Byte> inputImage, Image<Gray, Byte> templateImage)
+        {
+            bool success = false;
+            Point dftSize = new Point(inputImage.Width + (templateImage.Width * 2),
+                inputImage.Height + (templateImage.Height * 2));
+            using (Image<Gray, Byte> pad_array = new Image<Gray, Byte>(dftSize.X, dftSize.Y))
+            {
+                pad_array.ROI = new Rectangle(templateImage.Width, templateImage.Height, inputImage.Width,
+                    inputImage.Height);
+                CvInvoke.cvCopy(inputImage.Convert<Gray, Byte>(), pad_array, IntPtr.Zero);
+
+                pad_array.ROI = (new Rectangle(0, 0, dftSize.X, dftSize.Y));
+                using (Image<Gray, float> result_Matrix =
+                    pad_array.MatchTemplate(templateImage, TemplateMatchingType.CcoeffNormed))
+                {
+                    result_Matrix.ROI = new Rectangle(templateImage.Width, templateImage.Height, inputImage.Width,
+                        inputImage.Height);
+
+                    Point[] MAX_Loc, Min_Loc;
+                    double[] min, max;
+                    result_Matrix.MinMax(out min, out max, out Min_Loc, out MAX_Loc);
+
+                    using (Image<Gray, double> RG_Image = result_Matrix.Convert<Gray, double>().Copy())
+                    {
+                        //#TAG WILL NEED TO INCREASE SO THRESHOLD AT LEAST 0.8...used to have 0.7
+
+                        if (max[0] > 0.85)
+                        {
+                            success = true;
+                        }
+                    }
+                }
+            }
+
+            return success;
+        }
+
+        private void TextRecogniser(Image image)
+        {
+            Emgu.CV.OCR.Tesseract tesseract = new Emgu.CV.OCR.Tesseract
+                (@"F:\Working Pack\c#\Scripter\Scripter\Templates\eng.traineddata", "eng",
+                OcrEngineMode.TesseractLstmCombined);
+            var tempImg = new Bitmap(image).ToImage<Gray, byte>();
+            tesseract.SetImage(tempImg);
+            tesseract.Recognize();
+            string response = tesseract.GetUTF8Text();
+            tesseract.Dispose();
+        }
+    }
+}
